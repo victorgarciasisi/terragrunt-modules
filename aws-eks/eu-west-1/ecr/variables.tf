56b6e1898d9f63650a2c1f8c696f5dd6b70bf457
victorgarciasisi <45923592+victorgarciasisi@users.noreply.github.com>
2026-04-14 09:33:10 +0200

    Update variables.tf

--- aws-eks/eu-west-1/ecr/variables.tf
+++ aws-eks/eu-west-1/ecr/variables.tf
@@ -1,9 +1,3 @@
 variable "environment" {
   type = string
 }
-
-variable "ecr_users" {
-  description = "Usuarios IAM a los que se les dará acceso completo a ECR"
-  type        = list(string)
-  default     = []
-}
